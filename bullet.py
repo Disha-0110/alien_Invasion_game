24dfd98506ad3e4e66abb4f824c03647ad7b9916
Disha-0110 <dpatel85@student.cscc.edu>
2025-11-16 23:43:03 -0500

    Add files via upload

---
+++ bullet.py
@@ -0,0 +1,31 @@
+import pygame
+from pygame.sprite import Sprite
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
+
+class Bullet(Sprite):
+    def __init__(self,game: 'AlienInvasion'):
+        super().__init__()
+        
+        self.screen = game.screen
+        self.settings = game.settings
+        ship = game.ship
+
+        image = pygame.image.load(self.settings.bullet_file)
+        image = pygame.transform.scale(image,
+            (self.settings.bullet_w, self.settings.bullet_h)
+            )
+        self.image = pygame.transform.rotate(image, -90)
+
+        self.rect = self.image.get_rect()
+        self.rect.midleft = ship.rect.midright
+        self.x = float(self.rect.x)
+
+    def update(self):
+        self.x -= self.settings.bullet_speed
+        self.rect.x = self.x
+
+    def draw_bullet(self):
+        self.screen.blit(self.image, self.rect)
---
+++ ship.py
@@ -0,0 +1,52 @@
+import pygame
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from alien_invasion import AlienInvasion
+    from arsenal import Arsenal
+
+
+
+class Ship:
+
+
+    def __init__(self, game: 'AlienInvasion', arsenal: 'Arsenal'):
+        self.game = game
+        self.settings = game.settings
+        self.screen = game.screen
+        self.boundaries = self.screen.get_rect()
+
+        original = pygame.image.load(self.settings.ship_file)
+        original = pygame.transform.scale(original,
+            (self.settings.ship_w, self.settings.ship_h)
+            )
+        self.image = pygame.transform.rotate(original, -90)
+
+        self.rect = self.image.get_rect()
+        self.rect.midleft = self.boundaries.midleft
+        self.moving_up = False
+        self.moving_down = False
+        self.y = float(self.rect.y)
+        self.arsenal = arsenal 
+        
+    def update(self):
+
+        #updating the position of the ship
+        self._update_ship_movement()
+        self.arsenal.update_arsenal()
+
+    def _update_ship_movement(self):
+
+        temp_speed = self.settings.ship_speed
+        if self.moving_up and self.rect.top < self.boundaries.top:
+            self.y += temp_speed
+        if self.moving_down and self.rect.bottom > self.boundaries.bottom:
+            self.y -= temp_speed
+
+        self.rect.y = self.y
+
+    def draw(self):
+        self.arsenal.draw()
+        self.screen.blit(self.image, self.rect)
+    def fire(self):
+        return self.arsenal.fire_bullet()
